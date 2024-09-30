73a83a4af54b5c7e26a04f53ab3e9f293e3e51d7
Christian Maidana <christian.maidana@globant.com>
2024-09-30 10:06:37 -0300

    Add Email.js to Contact form

---
+++ js/mail.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', event => {
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach((c) => {
+
+        c.addEventListener('click', () => {
+            document.getElementById('form-modal').classList.remove('is-active');
+        });
+    });
+
+    (function () {
+        emailjs.init({
+            publicKey: "alQyTAhe863Gi-j4w",
+        });
+    })();
+
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const submitButton = document.getElementById('submit-button');
+        const modal = document.getElementById('form-modal');
+        submitButton.disabled = true;
+        submitButton.classList.add('is-loading');
+        setTimeout(() => {
+            submitButton.disabled = false;
+            submitButton.classList.remove('is-loading');
+            modal.classList.add('is-active');
+        }, 2000);
+        // emailjs.sendForm('mapesoftware', 'template_uz69bup', this)
+        //     .then(() => {
+        //         console.log('SUCCESS!');
+        //     }, (error) => {
+        //         console.log('FAILED...', error);
+        //     });
+    });
+});
